76c02eb234614d871293496eadfdae2e64ef32cb
cutesanghyun <43621856+cutesanghyun@users.noreply.github.com>
2020-06-13 12:01:08 +0900

    Ver.200613.02

---
+++ Train/2048.js
@@ -0,0 +1,89 @@
+let table = document.querySelector('#table')
+let data = [];
+
+function reset() {
+    let fragment = document.createDocumentFragment();
+    [1, 2, 3, 4].forEach(function () {
+        let horData = [];
+        data.push(horData);
+        let tr = document.createElement('tr');
+        [1, 2, 3, 4].forEach(function () {
+            horData.push(0);
+            let td = document.createElement('td');
+            tr.appendChild(td);
+        });
+        fragment.appendChild(tr);
+    });
+    table.appendChild(fragment);
+}
+
+function createRandNumber() {
+    let emptyList = [];
+    data.forEach(function (horData, i) {
+        horData.forEach(function (verData, j) {
+            if (!verData) {
+                emptyList.push([i, j]);
+            }
+        });
+    });    
+    let randomTd = emptyList[Math.floor(Math.random() * emptyList.length)];    
+    data[randomTd[0]][randomTd[1]] = Math.floor(Math.random()*10);
+    Draw();
+}
+function Draw() {
+    data.forEach(function (horData, i) {
+        horData.forEach(function (verData, j) {
+            if (verData > 0) {
+                table.children[i].children[j].textContent = verData;
+            } else {
+                table.children[i].children[j].textContent = '';
+            }
+        });
+    });
+}
+
+reset();
+createRandNumber();
+Draw();
+
+let startDrag = false;
+let dragging = false;
+let startPosion;
+let endPosition;
+window.addEventListener('mousedown', function (event) {
+    startDrag = true;
+    startPosion = [event.clientX, event.clientY];
+})
+window.addEventListener('mousemove', function (event) {
+    if (startDrag) {
+        dragging = true;
+    }
+})
+window.addEventListener('mouseup', function (event) {
+    startDrag = false;
+    endPosition = [event.clientX, event.clientY];
+    let direction;
+    let gapX = endPosition[0] - startPosion[0]; // 왼쪽이 마이너스 오른쪽이 플러스
+    let gapY = endPosition[1] - startPosion[1]; // 위쪽이 마이너스 아래쪽이 플러스
+    if (gapX < 0 && Math.abs(gapX) / Math.abs(gapY) > 1) {     //오 아, 아 왼, 왼 위, 위 오
+        direction = 'left';
+    } else if (gapX > 0 && Math.abs(gapX) / Math.abs(gapY) > 1) {
+        direction = 'right';
+    } else if (gapY < 0 && Math.abs(gapX) / Math.abs(gapY) < 1) {
+        direction = 'up';
+    } else if (gapY > 0 && Math.abs(gapX) / Math.abs(gapY) < 1) {
+        direction = 'down';
+    }
+    startDrag = false;
+    dragging = false;
+    switch (direction) {
+        case 'left':
+
+        case 'right':
+
+        case 'up':
+
+        case 'down':
+    }
+    createRandNumber();
+})
