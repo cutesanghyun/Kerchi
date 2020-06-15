f66c62de62a9f95c60265fa5815f2284b6f39ecf
cutesanghyun <cutesanghyun@naver.com>
2020-06-15 11:35:54 +0900

    Ver.200615.02

--- python/엑잘시세가져오기.py
+++ python/엑잘시세가져오기.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-url = 'http://play.afreecatv.com/coppag/224499973'
+url = 'https://poe.ninja/challenge/currency'
 
 driver = webdriver.Chrome()
 driver.get(url)
@@ -12,8 +12,21 @@ time.sleep(3)
 html = driver.page_source
 soup = BeautifulSoup(html)
 
-data = soup.find('video',{'id':'pipMedia'})
-print(data)
+data = soup.find('tbody').findAll('tr')
+global showprice
+for tr in data:
+    target = tr.findAll('img',{'title':'엑잘티드 오브'})
+    if target:
+        showprice = tr.find('span',{'class':'currency-amount'}).text
 
+print(showprice)
+driver.close()
+
+
+
+# myUrl = 'http://121.175.229.220/Kerchi/PathOfExile/index.html'
+
+# driver = webdriver.Chrome()
+# driver.get(myUrl)
 
 
