772cdef6f76ed3a1d048dc972733d354c688a649
cutesanghyun <cutesanghyun@naver.com>
2020-06-16 08:05:49 +0900

    Ver.200616.02

--- Train/계산기.js
+++ Train/계산기.js
@@ -1,11 +1,48 @@
-const result = document.querySelector('#result');
+let a;
+let b;
+let operator;
+let mathItUp = {
+    '+': function(a,b) {return a+b},
+    '-': function(a,b) {return a-b},
+    '*': function(a,b) {return a*b},
+    '/': function(a,b) {return a/b},
+}
 
 document.querySelector('#clear').addEventListener('click', () => {
     result.textContent = '';    
+    a = 0;
+    b = 0;
 });
 
 document.querySelector('#calculate').addEventListener('click', () => {
-    let inputValue = result.textContent;    
+    b = Number(result.textContent);
+    result.textContent = mathItUp[operator](a,b); 
+    a = result.textContent;
+    b = 0;
+});
+
+document.querySelector('#minus').addEventListener('click', () => {
+    a = Number(result.textContent);    
+    operator = '-';
+    result.textContent = '';      
+});
+
+document.querySelector('#plus').addEventListener('click', () => {
+    a = Number(result.textContent);    
+    operator = '+';
+    result.textContent = '';      
+});
+
+document.querySelector('#divide').addEventListener('click', () => {
+    a = Number(result.textContent);    
+    operator = '/';
+    result.textContent = '';      
+});
+
+document.querySelector('#multiply').addEventListener('click', () => {
+    a = Number(result.textContent);    
+    operator = '*';
+    result.textContent = '';      
 });
 
 document.querySelectorAll('.btn').forEach(function (e) {
--- Train/숫자야구.js
+++ Train/숫자야구.js
@@ -1,54 +1,67 @@
-let input = document.querySelector('#input')
-let calculate = document.querySelector('#calculate')
-let result = document.querySelector('#result')
+let input = document.querySelector('#input');
+let result = document.querySelector('#result');
+let check = document.querySelector('#check')
+let question = [];
+let strike = 0;
+let ball = 0;
+let count = 10;
 
-number0 = []
-for (i = 0; i < 10; i++) {
-    number0.push(i)
-}
-
-answer = []
-for (i = 0; i < 4; i++) {
-    randomNumber = Math.floor(Math.random() * (10 - i))
-    answer.push(number0[randomNumber])
-    number0.splice(randomNumber, 1)
-}
-console.log(answer);
-
-let count = 0
-calculate.addEventListener('click', () => {
-    if (count < 10) {
-        if (input.value.length === 4) {
-            if (answer.join('') === input.value) {
-                result.append('홈런~!!', document.createElement('br'))
-                result.append('새로운 게임을 하시려면 새로고침(f5)을 눌러주세요')
-                count = count + 10
-            } else {
-                strike = 0
-                ball = 0
-                for (i = 0; i < 4; i++) {
-                    if (answer[i] === Number(input.value.split('')[i])) {
-                        strike = strike + 1
-                    } else {
-                        for (ii = 0; ii < 4; ii++) {
-                            if (answer[i] === Number(input.value.split('')[ii])) {
-                                ball = ball + 1
-                            }
-                        }
-                    }
-                }                
-                result.append(`${input.value}: ${strike}스트라이크 ${ball}볼, 남은횟수:${10-count}회`, document.createElement('br'));
-            }
-            count = count + 1
-        } else {
-            result.append('4자리수를 입력해주세요', document.createElement('br'));
-        }
+function reset() {
+    count = 10;
+    question = [];
+    let baseNum = [...Array(9).keys()].map((v) => v + 1);
+    for (i = 0; i < 4; i++) {
+        let ranNum = Math.floor(Math.random() * baseNum.length);
+        question.push(baseNum.splice(ranNum, 1)[0]);
+        console.log(question);
     }
-    if (count === 10) {
-        result.append(`Game Over! 정답은 ${answer.join('')} 이었습니다.`, document.createElement('br'));
-        result.append('새로운 게임을 하시려면 새로고침(f5)을 눌러주세요')
-        count = count + 1
-    }  
-})
+}
+
+reset();
+
+check.addEventListener('click', () => {
+    let addDiv = document.createElement('div');
+    if (input.value.length === 4) {
+        if (count <= 0) {
+            addDiv.textContent = 'Game Over';
+            result.append(addDiv);            
+            setTimeout(function() {
+                result.innerHTML = '';
+                reset();
+            }, 2500);
+        } else if (Number(input.value) === Number(question.join(''))) {
+            addDiv.textContent = '홈런';
+            result.append(addDiv);        
+            setTimeout(function() {
+                result.innerHTML = '';
+                reset();
+            }, 2500);
+        } else {
+            for (i = 0; i < 4; i++) {
+                if (Number(input.value[i]) === question[i]) {
+                    strike = strike + 1;
+                } else if (Number(input.value[i]) !== question[i] && question.includes(Number(input.value[i]))) {
+                    ball = ball + 1;
+                }
+            }
+            count = count - 1;
+            addDiv.textContent = `${strike}strike ${ball}ball, 남은 시도: ${count}`
+            result.append(addDiv);
+            strike = 0;
+            ball = 0;
+        }
+    } else {
+        alert('4자리 수를 입력해주세요');
+    }
+});
+
+
+
+
+
+
+
+
+
 
 
